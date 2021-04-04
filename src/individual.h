9ba56b5dfc7b08d594ecf812e09854d6dc410484
Jiaan <jiaanliux@gmail.com>
2021-04-04 18:33:01 +0800

    test

---
+++ src/individual.h
@@ -0,0 +1,144 @@
+/*
+ * individual.h
+ *
+ *  Created on: 5 Mar 2020
+ *      Author: hinchr
+ */
+
+#ifndef INDIVIDUAL_H_
+#define INDIVIDUAL_H_
+
+/************************************************************************/
+/******************************* Includes *******************************/
+/************************************************************************/
+
+#include "structure.h"
+#include "params.h"
+#include "constant.h"
+
+/************************************************************************/
+/****************************** Structures  *****************************/
+/************************************************************************/
+
+typedef struct individual individual;
+
+struct individual{
+	long idx;
+	long house_no;
+	int age_group;
+	int age_type;
+	int occupation_network;
+
+	int base_random_interactions;
+	int random_interactions;
+	int n_interactions[MAX_DAILY_INTERACTIONS_KEPT];
+	interaction *interactions[MAX_DAILY_INTERACTIONS_KEPT];
+
+	int status;
+	double hazard;
+	double infectiousness_multiplier;
+	event *current_disease_event;
+	event *next_disease_event;
+	infection_event *infection_events;
+
+	int quarantined; //PROGRESS: check the references
+	event *quarantine_event;
+	event *quarantine_release_event;
+	int quarantine_test_result;
+	
+	trace_token *trace_tokens;
+	trace_token *index_trace_token;
+	event *index_token_release_event;
+	double traced_on_this_trace;
+
+	int app_user;
+
+	int ward_idx;
+	int ward_type;
+
+	int hospital_idx;
+
+	int hospital_state;
+	int disease_progression_predicted[N_HOSPITAL_WARD_TYPES];
+	event *current_hospital_event;
+	event *next_hospital_event;
+
+	int worker_type;
+
+	short vaccine_status;
+	short vaccine_status_next;
+	event *vaccine_wane_event;
+
+	int health_code_user;
+	int health_code_state;
+};
+
+struct interaction{
+	int type;
+	int network_id;
+	int traceable;
+	int manual_traceable;
+	individual *individual;
+	interaction *next;
+};
+
+struct interaction_block{
+	interaction *interactions;
+	interaction_block *next;
+};
+
+struct infection_event{
+	int *times;
+	individual *infector;
+	int infector_status;
+	int infector_hospital_state;
+	int infector_network;
+	int time_infected_infector;
+	infection_event *next;
+	int is_case;
+	int network_id;
+	float strain_multiplier;
+};
+
+/************************************************************************/
+/******************************  Macros**** *****************************/
+/************************************************************************/
+
+#define time_symptomatic( indiv ) ( max( indiv->infection_events->times[SYMPTOMATIC], indiv->infection_events->times[SYMPTOMATIC_MILD] ) )
+#define time_infected( indiv ) ( max( max( indiv->infection_events->times[PRESYMPTOMATIC], indiv->infection_events->times[ASYMPTOMATIC ] ), indiv->infection_events->times[PRESYMPTOMATIC_MILD] ) )
+#define time_infected_infection_event( infection_event ) ( max( max( infection_event->times[PRESYMPTOMATIC], infection_event->times[ASYMPTOMATIC ] ), infection_event->times[PRESYMPTOMATIC_MILD] ) )
+
+#define is_in_hospital( indiv ) ( ( indiv->status == HOSPITALISED || indiv->status == CRITICAL || indiv->status == HOSPITALISED_RECOVERING ) )
+#define not_in_hospital( indiv ) ( (indiv->hospital_state == NOT_IN_HOSPITAL) || (indiv->hospital_state == DISCHARGED) )
+
+#define vaccine_protected( indiv ) ( (indiv->vaccine_status == VACCINE_PROTECTED_FULLY) || (indiv->vaccine_status == VACCINE_PROTECTED_SYMPTOMS ) )
+
+/************************************************************************/
+/******************************  Functions  *****************************/
+/************************************************************************/
+
+void initialize_individual( individual*, parameters*, long );
+void initialize_hazard( individual*, parameters* );
+void set_age_group( individual*, parameters*, int );
+void set_house_no( individual*, long );
+void set_quarantine_status( individual*, parameters*, int, int, model* );
+void set_recovered( individual*, parameters*, int , model *);
+void set_susceptible( individual*, parameters*, int );
+void set_hospitalised( individual*, parameters*, int );
+void set_hospitalised_recovering( individual*, parameters*, int );
+void set_critical( individual*, parameters*, int );
+void set_dead( individual*, parameters*, int );
+void set_case( individual*, int );
+void set_waiting( individual*, parameters*, int );
+void set_general_admission( individual*, parameters*, int );
+void set_icu_admission( individual*, parameters*, int );
+void set_mortuary_admission( individual*, parameters*, int );
+void set_discharged( individual*, parameters*, int );
+void set_vaccine_status( individual*, short, short );
+void transition_vaccine_status( individual* );
+void update_random_interactions( individual*, parameters* );
+int count_infection_events( individual * );
+void destroy_individual( individual* );
+void print_individual( model *, long );
+
+#endif /* INDIVIDUAL_H_ */
